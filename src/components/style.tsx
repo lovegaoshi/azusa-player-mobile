a0b157eda949ed2a7210016a1f0bd9d88e69ef54
Ubuntu <gaoshi@noxplay.us.to>
2023-05-11 21:31:25 +0000

    feat: skin test

--- src/components/style.tsx
+++ src/components/style.tsx
@@ -1,4 +1,46 @@
 import { StyleSheet } from 'react-native';
+import noxTheme from './styles/nox';
+import azusaTheme from './styles/azusa';
+import { notNullDefault as nd } from '../utils/Utils';
+import style from './styles/styleInterface';
+
+export const createStyle = (customStyle: style) => {
+  const refTheme = azusaTheme; // customStyle.lightTheme ? azusaTheme : noxTheme;
+  return StyleSheet.create({
+    screenContainer: {
+      flex: 1,
+      backgroundColor: nd(customStyle.screenBackgroundColor, refTheme.screenBackgroundColor),
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    contentContainer: {
+      flex: 6,
+      alignItems: 'center',
+    },
+    topBarContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      // paddingHorizontal: 20,
+      paddingLeft: 10,
+      paddingRight: 5,
+      justifyContent: 'flex-end',
+      top: 4,
+    },
+    actionRowContainer: {
+      flexDirection: 'column',
+      width: '96%',
+      paddingLeft: 5,
+      paddingright: 5,
+      left: '2%',
+      alignItems: 'center',
+      height: 140,
+    },
+    playlistDrawer: {
+      backgroundColor: nd(customStyle.playlistDrawerBackgroundColor, refTheme.playlistDrawerBackgroundColor),
+    }
+  });
+  
+}
 
 export const styles = StyleSheet.create({
   screenContainer: {
