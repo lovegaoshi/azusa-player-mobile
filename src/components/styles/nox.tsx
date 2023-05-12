0b061680ffdd055d5ae16fc82ac1749c37b745ce
Ubuntu <gaoshi@noxplay.us.to>
2023-05-12 02:09:53 +0000

    feat: style test

---
+++ src/components/styles/nox.tsx
@@ -0,0 +1,12 @@
+import style from './styleInterface';
+export default {
+    metaData: {
+        themeName: 'Nox Theme',
+        themeDesc: 'Nox Theme',
+        themeAuthor: 'lovegaoshi@github',
+    },
+    lightTheme: false,
+    screenBackgroundColor: 'grey',
+    playlistDrawerBackgroundColor: 'green',
+
+} as style;
