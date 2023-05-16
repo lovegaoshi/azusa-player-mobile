346e0fadabdb65dbe072401a8b95a3a3f01531c8
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-05-15 22:17:58 -0700

    feat: i18n

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -1,23 +1,24 @@
 // src/localization/i18n.ts
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import translationEN from './en/translation.json'
-import translationZHCN from './zhcn/translation.json'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationEN from './en/translation.json';
+import translationZHCN from './zhcn/translation.json';
 
 export const resources = {
-    zhcn: {
-        translation: translationZHCN,
-      },
-      en: {
+  zhcn: {
+    translation: translationZHCN,
+  },
+  en: {
     translation: translationEN,
   },
-}
+};
 
 i18n.use(initReactI18next).init({
   resources,
+  compatibilityJSON: 'v3',
   lng: 'zhcn',
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
-})
+});
