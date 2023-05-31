28402e5fa8f3cc3a299d7b72491876223c828658
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-05-30 20:42:31 -0700

    refactor: file organization
    prepare to add a discover panel. even though i dont want to build one.
    the idea is to take out the bili login and settings draweritem to give up some screen realestate. this requires us to make a react-nav fork.

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ const App: React.FC = () => {
                   component={Bilibili}
                 />
                 <Drawer.Screen
-                  name={ViewEnum.LEFT_DRAWER}
+                  name={ViewEnum.SETTINGS}
                   options={{
                     drawerIcon: () => <IconButton icon="cog" />,
                     title: String(t('appDrawer.settingScreenName')),
--- src/enums/View.ts
+++ src/enums/View.ts
@@ -1,7 +1,8 @@
 export enum ViewEnum {
   PLAYER_HOME = 'NoxHome',
-  LEFT_DRAWER = 'LeftDrawer',
+  SETTINGS = 'Settings',
   PLAYER_COVER = 'NoxCover',
   PLAYER_PLAYLIST = 'NoxPlaylist',
   USER_LOGIN = 'NoxLogin',
+  EXPORE = 'Explore',
 }
