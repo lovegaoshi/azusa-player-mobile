be38d8074b73af9d6b9aaaec2807a7dbc7388724
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-05-31 06:09:04 -0700

    style: drawer UI change

--- src/enums/View.ts
+++ src/enums/View.ts
@@ -1,8 +1,8 @@
 export enum ViewEnum {
   PLAYER_HOME = 'NoxHome',
-  SETTINGS = 'Settings',
+  SETTINGS = 'NoxSettings',
   PLAYER_COVER = 'NoxCover',
   PLAYER_PLAYLIST = 'NoxPlaylist',
   USER_LOGIN = 'NoxLogin',
-  EXPORE = 'Explore',
+  EXPORE = 'NoxExplore',
 }
