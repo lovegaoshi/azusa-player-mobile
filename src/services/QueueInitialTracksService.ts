620d1984ce3b89cc9746ad82bc6b73fc0896a9be
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-06-04 03:15:35 -0700

    refactor: code cleanup

--- src/services/QueueInitialTracksService.ts
+++ src/services/QueueInitialTracksService.ts
@@ -1,20 +1,3 @@
-import TrackPlayer, { Track } from 'react-native-track-player';
-
-import playlistData from '../assets/data/playlist.json';
-// @ts-expect-error – sure we can import this
-import localTrack from '../assets/resources/pure.m4a';
-// @ts-expect-error – sure we can import this
-import localArtwork from '../assets/resources/artwork.jpg';
-
 export const QueueInitialTracksService = async (): Promise<void> => {
-  await TrackPlayer.add([
-    ...(playlistData as Track[]),
-    {
-      url: localTrack,
-      title: 'Pure (Demo)',
-      artist: 'David Chavez',
-      artwork: localArtwork,
-      duration: 28,
-    },
-  ]);
+  return;
 };
