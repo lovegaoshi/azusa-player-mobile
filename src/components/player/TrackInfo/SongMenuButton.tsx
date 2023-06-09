a4943a4a45f66681bfd713951e0431094e0796d3
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-06-08 22:31:58 -0700

    fix: recommended song menu
    bilisuggest will add songs that doesnt belong in the current playlist into the current playing list (confusing, i know). instead of retriving from the current playlist, return the song instead directly from track.song to prevent most null problems.

--- src/components/player/TrackInfo/SongMenuButton.tsx
+++ src/components/player/TrackInfo/SongMenuButton.tsx
@@ -48,6 +48,7 @@ export default ({ track }: props) => {
         disabled={song === undefined}
       ></IconButton>
       <SongMenu
+        song={song}
         songMenuVisible={songMenuVisible}
         setSongMenuVisible={setSongMenuVisible}
         songMenuSongIndexes={[songMenuSongIndex]}
