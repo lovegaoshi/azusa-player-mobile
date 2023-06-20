289e1f69b409b9a3906a41f70ba55d012a5c4230
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-06-19 21:59:58 -0700

    feat: wisteria splash
    怎么build fail 急急急我是吉吉国王

--- src/components/styles/SteriaTheme.js
+++ src/components/styles/SteriaTheme.js
@@ -1,33 +1,30 @@
 export default {
   metaData: {
-    themeName: '安妮播放器',
-    themeDesc: '关注弃车人的骄傲你练直播间27484357',
-    themeAuthor: '食梦莲lotus@bilibili',
-    themeIcon:
-      'https://i0.hdslb.com/bfs/face/81e39eb61a0cbefeb7a6647137167011a63e717f.jpg',
+    themeName: 'guraaaaaa',
+    themeDesc: 'A',
+    themeAuthor: 'lovegaoshi',
+    themeIcon: 'https://pbs.twimg.com/media/FLLR87OakAAs3Li.jpg',
     darkTheme: false,
   },
 
   gifs: [
-    'https://article.biliimg.com/bfs/article/313c42ec86c5bdaa13ba3d8a5633e696bfd2412f.gif',
-    'https://article.biliimg.com/bfs/article/f961bb02daccadcc19daf191e5540e633fe4b104.gif',
-    'https://article.biliimg.com/bfs/article/5fb01231dff56e8b089126c6edfa6fc68c7b2be7.gif',
-    'https://article.biliimg.com/bfs/article/8dc01b02cd7dfeb8871dea607b4f191a5be80f64.gif',
-    'http://i0.hdslb.com/bfs/garb/e0434d5ee3ad0be34377ac0df78a6075cabf34e8.png',
-    'http://i0.hdslb.com/bfs/garb/e4bc12886185357562cd59d8d18257a8ad19d9e1.png',
-    'http://i0.hdslb.com/bfs/garb/9cf9238d185de745850b6b7c0468bb0654e9c30e.png',
-    'http://i0.hdslb.com/bfs/garb/e16087b121cb48dd34bab5a02ffc5601149b5ca4.png',
-    'http://i0.hdslb.com/bfs/garb/802462836f55a35bf09d6ad00e4ba52ed101d2f6.png',
-    'http://i0.hdslb.com/bfs/garb/22113e3f01f64e781754bf1297d3d201fb97a92c.png',
-    'http://i0.hdslb.com/bfs/garb/bf79e7b0df21f7a75a640ca5a6e76c9ed687acd3.png',
-    'http://i0.hdslb.com/bfs/garb/f4d9ae969e692ea086a4382698cf78f651bbc975.png',
-    'http://i0.hdslb.com/bfs/garb/c2d0bac0d3d98b3424cd58d6e68d79bd352359f6.png',
-    'http://i0.hdslb.com/bfs/garb/2cceff64dab9ab0d1ec389bda04655390fe4aea4.png',
-    'http://i0.hdslb.com/bfs/garb/ec5cc3729cffbfd6acafa9f6c7b9171f585299e3.png',
-    'http://i0.hdslb.com/bfs/garb/74bbe0803168dc873a7e97334b0bab85d337a39c.png',
+    'http://i0.hdslb.com/bfs/live/e8073adeb52036d0d563c848c4b55b8449bc4b85.png',
+    'http://i0.hdslb.com/bfs/live/f2a7a0916015a741a192ae85ee593a39c6dd04a7.png',
+    'http://i0.hdslb.com/bfs/live/0a691aec40c738918014b27acfefa3b295b8a458.png',
+    'http://i0.hdslb.com/bfs/live/cdb1f3adeee987c1fe5303ca90443932edb23d30.png',
+    'http://i0.hdslb.com/bfs/live/c9450e570d7abcf5a920b65e18b9624b75c9d4fb.png',
+    'http://i0.hdslb.com/bfs/live/021761abdfe8dc417e5267b74877bebd43dcfd58.png',
+    'http://i0.hdslb.com/bfs/live/e0ae55eb80c6b7c01eeb042a26a3bd2938ed679a.png',
+    'http://i0.hdslb.com/bfs/live/bea985bcf662dc4d85c9f78633c57398b7c3d223.png',
+    'http://i0.hdslb.com/bfs/live/5b29c4c15e6b97da2df564899f84906fc800590b.png',
+    'http://i0.hdslb.com/bfs/live/a9fa62db7b7233dac30d3abd851b992710bc1649.png',
   ],
   backgroundImages: [
-    'https://koistatic.terrach.net/dbs-art/2022/08/20220824144703485.jpg',
+    {
+      type: 'video',
+      identifier:
+        'https://cdn.donmai.us/original/ed/59/__gawr_gura_and_bloop_hololive_and_1_more_drawn_by_mokyuko_and_shikabashi_joxx__ed598bec0a9c3be5a9835c441ca40910.mp4',
+    },
   ],
 
   customColors: {
@@ -36,24 +33,24 @@ export default {
     // note its transparency STACKS with colors.background
     maskedBackgroundColor: 'rgba(255, 255, 255, 0.40)',
     // playlist background when selected. should be the contrast color of the primary text color.
-    playlistDrawerBackgroundColor: '#ffa6c9',
+    playlistDrawerBackgroundColor: '#FFDA1F',
     // playlist background when selected. should be the contrast color of the primary text color.
     playlistDrawerBackgroundColorTransparent: 'rgba(255, 166, 201, 0.35)',
     // progress bar's round icon color.
-    progressThumbTintColor: '#ff1493',
+    progressThumbTintColor: '#1e90ff',
     // progresss bar's whatever's left of the round icon
-    progressMinimumTrackTintColor: '#ff1493',
+    progressMinimumTrackTintColor: '#1e90ff',
     // progresss bar's whatever's right of the round icon
     progressMaximumTrackTintColor: '#FFFFFF',
   },
 
   colors: {
-    text: '#ff1493',
-    primary: '#ff1493',
+    text: '#1e90ff',
+    primary: '#1e90ff',
     onPrimary: 'rgb(255, 255, 255)',
     primaryContainer: 'rgb(255, 217, 223)',
     onPrimaryContainer: 'rgb(63, 0, 22)',
-    secondary: '#cc3366',
+    secondary: '#00aaff',
     onSecondary: 'rgb(255, 255, 255)',
     secondaryContainer: 'rgb(255, 217, 223)',
     onSecondaryContainer: 'rgb(43, 21, 26)',
@@ -66,11 +63,11 @@ export default {
     errorContainer: 'rgb(255, 218, 214)',
     onErrorContainer: 'rgb(65, 0, 2)',
     background: 'rgba(255, 251, 255, 0.3)',
-    onBackground: '#ff1493',
+    onBackground: '#1e90ff',
     surface: 'rgb(255, 251, 255)',
-    onSurface: '#ff1493',
-    surfaceVariant: 'rgb(243, 221, 224)',
-    onSurfaceVariant: '#ff1493',
+    onSurface: '#1e90ff',
+    surfaceVariant: 'rgba(255, 251, 255, 0.2)',
+    onSurfaceVariant: '#1e90ff',
     outline: 'rgb(132, 115, 117)',
     outlineVariant: 'rgb(214, 194, 196)',
     shadow: 'rgba(0, 0, 0)',
