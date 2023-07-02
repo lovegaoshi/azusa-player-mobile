2c9a53368aa6fd98bd9ed80209fe3d8eb034d473
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-07-02 09:46:43 -0700

    feat: splash ads

---
+++ src/components/setting/SplashSettings.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import { Pressable } from 'react-native';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { List, MD3Colors, IconButton, Text } from 'react-native-paper';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useTranslation } from 'react-i18next';
+import FastImage from 'react-native-fast-image';
+import { Dimensions } from 'react-native';
+
+import { useNoxSetting } from '../../hooks/useSetting';
+import { localSplashes } from '../background/AppOpenSplash';
+
+const style = {
+  flex: 1,
+  height: Dimensions.get('window').height,
+  width: Dimensions.get('window').width,
+};
+
+export default () => {
+  const playerStyle = useNoxSetting(state => state.playerStyle);
+  const [index, setIndex] = React.useState(0);
+
+  const nextImage = () => {
+    setIndex(index < localSplashes.length - 1 ? index + 1 : 0);
+  };
+
+  return (
+    <Pressable onPress={nextImage} style={style}>
+      <FastImage
+        source={localSplashes[index]()}
+        resizeMode={FastImage.resizeMode.contain}
+        style={style}
+      />
+    </Pressable>
+  );
+};
