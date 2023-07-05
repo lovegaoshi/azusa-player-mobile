1a780f630b1e9d58986b8741bea3052c1da9c13a
Ubuntu <gaoshi@noxplay.us.to>
2023-07-05 22:48:29 +0000

    feat: clear orphan cache

---
+++ src/hooks/useCleanCache.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import { useNoxSetting } from './useSetting';
+import NoxCache from '../utils/Cache';
+
+const useCleanCache = () => {
+  const playlists = useNoxSetting(state => state.playlists);
+  const playlistIds = useNoxSetting(state => state.playlistIds);
+  const [orphanedCache] = useState(
+    NoxCache.noxMediaCache!.getOrphanedCache(
+      playlistIds.reduce(
+        (acc, curr) => acc.concat(playlists[curr].songList),
+        [] as NoxMedia.Song[]
+      )
+    )
+  );
+
+  const cleanOrphanedCache = () => {
+    NoxCache.noxMediaCache?.cleanOrphanedCache(orphanedCache);
+  };
+
+  return { orphanedCache, cleanOrphanedCache };
+};
+
+export default useCleanCache;
