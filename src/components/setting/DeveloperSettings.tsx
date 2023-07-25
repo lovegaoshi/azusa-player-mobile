c97aefca0f8c3cc752443669484d937320a6a8c6
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-07-24 21:23:27 -0700

    fix: refresh clean orphan cache

--- src/components/setting/DeveloperSettings.tsx
+++ src/components/setting/DeveloperSettings.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { View, ScrollView } from 'react-native';
 import { List } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
+// eslint-disable-next-line import/no-unresolved
 import { APPSTORE } from '@env';
 
 import { useNoxSetting } from '../../hooks/useSetting';
-import { logStore, LOGLEVEL } from '../../utils/Logger';
+import { logStore, LOGLEVEL, getLog } from '../../utils/Logger';
 import GenericSelectDialog from '../dialogs/GenericSelectDialog';
 import useRenderSettingItem from './useRenderSetting';
 import useVersionCheck from '../../hooks/useVersionCheck';
-import { getLog } from '../../utils/Logger';
 import useAlert from '../dialogs/useAlert';
 import {
   SelectSettingEntry,
