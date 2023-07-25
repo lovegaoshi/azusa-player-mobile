43e5aca2498870269aa8b08f3d741cc273cb0b58
lovegaoshi <gaoshi@noxplay.us.to>
2023-07-25 21:44:47 +0000

    refactor: useRenderSetting wtf am i doing

--- src/components/setting/useRenderSetting.tsx
+++ src/components/setting/useRenderSetting.tsx
@@ -11,6 +11,110 @@ import { v4 as uuidv4 } from 'uuid';
 import { useNoxSetting } from '../../hooks/useSetting';
 import { SettingEntry } from './SetttingEntries';
 
+/**
+ * renders a generic clickable item.
+ * @param icon icon string.
+ * @param settingName subcategory in i18n.
+ * @param onPress callback on pressed
+ * @param settingCategory category in i18n.
+ * @returns
+ */
+const SettingListItem = (
+  icon: string,
+  settingName: string,
+  onPress: () => void,
+  settingCategory = 'DeveloperSettings',
+  modifyDescription = (val: string) => val
+) => {
+  const { t } = useTranslation();
+  const playerStyle = useNoxSetting(state => state.playerStyle);
+
+  return (
+    <List.Item
+      left={props => <IconButton icon={icon} size={40} />}
+      title={String(t(`${settingCategory}.${settingName}Name`))}
+      description={modifyDescription(
+        t(`${settingCategory}.${settingName}Desc`)
+      )}
+      onPress={onPress}
+      titleStyle={{ color: playerStyle.colors.primary }}
+      descriptionStyle={{ color: playerStyle.colors.secondary }}
+      style={styles.listItem}
+    />
+  );
+};
+
+const BooleanSetting = ({
+  settingName,
+  settingCategory,
+  reRender = false,
+  checkbox = false,
+}: SettingEntry) => {
+
+  const { t } = useTranslation();
+  const playerStyle = useNoxSetting(state => state.playerStyle);
+  const playerSetting = useNoxSetting(state => state.playerSetting);
+  const setPlayerSetting = useNoxSetting(state => state.setPlayerSetting);
+
+  const togglePlaylistReRender = useNoxSetting(
+    state => state.togglePlaylistShouldReRender
+  );
+
+  const saveSettings = (toggled: { [key: string]: any }) => {
+    setPlayerSetting(toggled);
+  };
+
+  const onToggle = () => {
+    saveSettings({ [settingName]: !playerSetting[settingName] });
+    if (reRender) {
+      togglePlaylistReRender();
+    }
+  };
+
+  return (
+    <TouchableRipple
+      onPress={onToggle}
+      style={styles.touchableRipple}
+      key={uuidv4()}
+    >
+      <View style={styles.settingContainer}>
+        <View style={styles.settingTextContainer}>
+          <Text
+            style={[
+              styles.settingText,
+              { color: playerStyle.colors.primary },
+            ]}
+          >
+            {t(`${settingCategory}.${settingName}Name`)}
+          </Text>
+          <Text
+            style={[
+              styles.settingDescription,
+              { color: playerStyle.colors.secondary },
+            ]}
+          >
+            {t(`${settingCategory}.${settingName}Desc`)}
+          </Text>
+        </View>
+        <View style={styles.switchContainer}>
+          {checkbox ? (
+            <Checkbox
+              status={playerSetting[settingName] ? 'checked' : 'unchecked'}
+              onPress={onToggle}
+            />
+          ) : (
+            <Switch
+              value={playerSetting[settingName]}
+              onValueChange={onToggle}
+              color={playerStyle.colors.onSurfaceVariant}
+            />
+          )}
+        </View>
+      </View>
+    </TouchableRipple>
+  );
+};
+
 export default () => {
   const { t } = useTranslation();
   const playerStyle = useNoxSetting(state => state.playerStyle);
