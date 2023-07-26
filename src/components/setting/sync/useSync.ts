8db073cdd76595c3f2edc561c018e1a7d6bf2b47
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-07-26 06:47:11 -0700

    feat: useSync

--- src/components/setting/sync/useSync.ts
+++ src/components/setting/sync/useSync.ts
@@ -3,6 +3,7 @@ import Snackbar from 'react-native-snackbar';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
+import { strFromU8, decompressSync } from 'fflate';
 
 import { loginDropbox, noxBackup, noxRestore } from './DropboxAuth';
 import { useNoxSetting } from '@hooks/useSetting';
@@ -13,7 +14,21 @@ const useSync = () => {
   const { t } = useTranslation();
   const initPlayer = useNoxSetting(state => state.initPlayer);
 
-  const restoreFromUint8Array = (res: Uint8Array) => {};
+  const importNoxExtensionContent = () => {};
+
+  const restoreFromUint8Array = (content: Uint8Array) => {
+    let parsedContent;
+    try {
+      parsedContent = JSON.parse(strFromU8(decompressSync(content)));
+    } catch (error) {
+      logger.error('parsed content is not a valid compressed JSON.');
+      Snackbar.show({
+        text: t('Sync.fileNotValidJson'),
+      });
+      return;
+    }
+    parsedContent;
+  };
 };
 
 export default useSync;
