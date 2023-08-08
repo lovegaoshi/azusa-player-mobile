82f9236cbef2b94332d9c7b0d898c28487fe389a
lovegaoshi <gaoshi@noxplay.us.to>
2023-08-08 22:59:22 +0000

    feat: skinSetting animation

--- src/components/setting/SkinSettings.tsx
+++ src/components/setting/SkinSettings.tsx
@@ -7,12 +7,19 @@ import {
   TouchableRipple,
   RadioButton,
 } from 'react-native-paper';
+import Animated, {
+  Layout,
+  LightSpeedInLeft,
+  LightSpeedOutRight,
+} from 'react-native-reanimated';
+
 import SkinSearchbar from './SkinSearchbar';
 import { useNoxSetting } from '@hooks/useSetting';
 import AzusaTheme from '../styles/AzusaTheme';
 // import AzusaTheme from '../styles/SteriaTheme';
 import NoxTheme from '../styles/NoxTheme';
 import { getUniqObjects } from '@utils/Utils';
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface DisplayTheme extends NoxTheme.style {
   builtin: boolean;
@@ -53,55 +60,63 @@ const SkinItem = ({ skin, checked, setChecked }: SkinItemProps) => {
     setPlayerStyles(playerStyles.filter(pSkin => pSkin !== skin));
 
   return (
-    <TouchableRipple onPress={selectTheme}>
-      <View style={styles.skinItemContainer}>
-        <View style={styles.skinItemLeftContainer}>
-          <Image
-            source={{ uri: skin.metaData.themeIcon }}
-            style={styles.skinItemImage}
-          />
-          <View style={styles.skinItemTextContainer}>
-            <Text
-              variant={'titleMedium'}
-              style={styles.skinTitleText}
-              numberOfLines={1}
-              ellipsizeMode="tail"
-            >{`${skin.metaData.themeName} by ${skin.metaData.themeAuthor}`}</Text>
-            <Text
-              variant={'labelLarge'}
-              style={{
-                color: playerStyle.colors.secondary,
-                maxWidth: '90%',
-              }}
-            >
-              {skin.metaData.themeDesc}
-            </Text>
-            <View style={styles.lightbulbContainer}>
-              <IconButton
-                icon={
-                  skin.metaData.darkTheme ? 'lightbulb-outline' : 'lightbulb-on'
-                }
-                size={25}
-                style={styles.lightbulbIcon}
-              />
+    <Animated.View
+      entering={LightSpeedInLeft}
+      exiting={LightSpeedOutRight}
+      layout={Layout.springify()}
+    >
+      <TouchableRipple onPress={selectTheme}>
+        <View style={styles.skinItemContainer}>
+          <View style={styles.skinItemLeftContainer}>
+            <Image
+              source={{ uri: skin.metaData.themeIcon }}
+              style={styles.skinItemImage}
+            />
+            <View style={styles.skinItemTextContainer}>
+              <Text
+                variant={'titleMedium'}
+                style={styles.skinTitleText}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >{`${skin.metaData.themeName} by ${skin.metaData.themeAuthor}`}</Text>
+              <Text
+                variant={'labelLarge'}
+                style={{
+                  color: playerStyle.colors.secondary,
+                  maxWidth: '90%',
+                }}
+              >
+                {skin.metaData.themeDesc}
+              </Text>
+              <View style={styles.lightbulbContainer}>
+                <IconButton
+                  icon={
+                    skin.metaData.darkTheme
+                      ? 'lightbulb-outline'
+                      : 'lightbulb-on'
+                  }
+                  size={25}
+                  style={styles.lightbulbIcon}
+                />
+              </View>
             </View>
           </View>
+          <View style={styles.skinItemRightContainer}>
+            <RadioButton
+              value={themeID}
+              status={checked === themeID ? 'checked' : 'unchecked'}
+              onPress={selectTheme}
+            />
+            <IconButton
+              icon="trash-can"
+              style={styles.deleteButton}
+              onPress={deleteTheme}
+              disabled={skin.builtin}
+            />
+          </View>
         </View>
-        <View style={styles.skinItemRightContainer}>
-          <RadioButton
-            value={themeID}
-            status={checked === themeID ? 'checked' : 'unchecked'}
-            onPress={selectTheme}
-          />
-          <IconButton
-            icon="trash-can"
-            style={styles.deleteButton}
-            onPress={deleteTheme}
-            disabled={skin.builtin}
-          />
-        </View>
-      </View>
-    </TouchableRipple>
+      </TouchableRipple>
+    </Animated.View>
   );
 };
 
@@ -135,13 +150,16 @@ const SkinSettings = () => {
       ]}
     >
       <SkinSearchbar onSearched={loadCustomSkin} />
-      <FlatList
-        data={allThemes}
-        renderItem={({ item }: { item: DisplayTheme }) => (
-          <SkinItem skin={item} checked={checked} setChecked={setChecked} />
-        )}
-        keyExtractor={item => getThemeID(item)}
-      />
+      <ScrollView>
+        {allThemes.map(item => (
+          <SkinItem
+            skin={item}
+            checked={checked}
+            setChecked={setChecked}
+            key={getThemeID(item)}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
