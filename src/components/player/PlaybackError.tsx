2550a301c244a3c7cd97a86c5e53d15941252151
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-09-02 20:04:47 -0700

    fix: error location

--- src/components/player/PlaybackError.tsx
+++ src/components/player/PlaybackError.tsx
@@ -12,10 +12,12 @@ export const PlaybackError: React.FC<{ error?: string }> = ({ error }) => {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    position: 'absolute',
   },
   text: {
     color: 'red',
     width: '100%',
     textAlign: 'center',
+    marginTop: -15,
   },
 });
