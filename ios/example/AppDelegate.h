e882fe3b0a26196320c896c43e11f3a707ec6314
lovegaoshi <gaoshi@noxplay.us.to>
2023-09-22 18:51:03 +0000

    fix(ios): oauth

--- ios/example/AppDelegate.h
+++ ios/example/AppDelegate.h
@@ -1,7 +1,11 @@
 #import <Expo/Expo.h>
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTLinkingManager.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : EXAppDelegateWrapper
+@interface AppDelegate : EXAppDelegateWrapper <RNAppAuthAuthorizationFlowManager>
+
+@property(nonatomic, weak) id<RNAppAuthAuthorizationFlowManagerDelegate> authorizationFlowManagerDelegate;
 
 @end
--- ios/example/AppDelegate.mm
+++ ios/example/AppDelegate.mm
@@ -23,4 +23,14 @@
 #endif
 }
 
+- (BOOL) application: (UIApplication *)application
+             openURL: (NSURL *)url
+             options: (NSDictionary<UIApplicationOpenURLOptionsKey, id> *) options
+{
+  if ([self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:url]) {
+    return YES;
+  }
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
