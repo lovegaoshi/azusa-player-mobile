ee06f570eafa4b6a439f455580022a159e4e3ebb
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-11-02 07:31:41 -0700

    fix: disable codepush on appstore
    with power, comes with great responsibility, and its a power i can not wield:( sorry codepush, ive disappointed you.

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ import 'react-native-gesture-handler';
 import 'react-native-url-polyfill/auto';
 import codePush from "react-native-code-push";
 
+import { APPSTORE } from '@env';
 import App from './src/App';
 import { PlaybackService } from './src/services';
 import { name as appName } from './app.json';
 import Share from './src/components/share/Share';
 
-AppRegistry.registerComponent(appName, () => codePush(App));
+AppRegistry.registerComponent(appName, () => APPSTORE ? App : codePush(App));
 AppRegistry.registerComponent('ShareMenuModuleComponent', () => Share);
 TrackPlayer.registerPlaybackService(() => PlaybackService);
