d4b738719645b788a695337f298f08c983c0f326
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-11-02 08:41:15 -0700

    chore: disable codepush
    codepush has a problem with asset bundler that RNTP doesnt show custom icons anymore..? needs further invesigation but turning off codepush for now :(

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ import 'react-native-gesture-handler';
 import 'react-native-url-polyfill/auto';
 import codePush from "react-native-code-push";
 
-import { APPSTORE } from '@env';
 import App from './src/App';
 import { PlaybackService } from './src/services';
 import { name as appName } from './app.json';
 import Share from './src/components/share/Share';
 
-AppRegistry.registerComponent(appName, () => APPSTORE ? App : codePush(App));
+AppRegistry.registerComponent(appName, () => App); // codePush(App)
 AppRegistry.registerComponent('ShareMenuModuleComponent', () => Share);
 TrackPlayer.registerPlaybackService(() => PlaybackService);
