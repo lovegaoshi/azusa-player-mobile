981d8fb047ad69a72c8451dc8e7ed5f2937f5789
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-11-26 09:59:53 -0800

    refactor: appStore

---
+++ src/stores/initializeStores.ts
@@ -0,0 +1,5 @@
+import { NativeModules, Platform } from 'react-native';
+import { useNoxSetting } from '@hooks/useSetting';
+
+const useInitializeStore = () => {};
+export default useInitializeStore;
