a48db80630af363997575457948a7bdaf2952c80
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-11-29 07:02:30 -0800

    feat: reorder util

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -132,3 +132,12 @@ export const filterUndefined = <T, K>(
     const val = myFunc(v);
     return val ? [val] : [];
   });
+
+export const reorder = <T>(list: T[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  if (removed === undefined) return result;
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
