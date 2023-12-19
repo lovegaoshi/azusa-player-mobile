c6f48104225f1eccd4597017294cc4ad14785e61
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-19 14:03:54 -0800

    feat: playlist sort

--- src/utils/playlistOperations.ts
+++ src/utils/playlistOperations.ts
@@ -14,3 +14,69 @@ export const updatePlaylistSongs = (
     .filter(v => !removeSongsId.includes(v.id));
   return playlist;
 };
+
+export const sortPlaylist = (
+  playlist: NoxMedia.Playlist,
+  sort: SORT_OPTIONS = SORT_OPTIONS.PREVIOUS_ORDER
+): NoxMedia.Playlist => {
+  playlist.sort = sort;
+  if (SORT_OPTIONS.PREVIOUS_ORDER === sort) {
+    // first get the largest order number in the songlist:
+    let largestOrder = 0;
+    let songsWithoutOrder = 1;
+    for (const song of playlist.songList) {
+      if (!song.order) {
+        songsWithoutOrder += 1;
+      } else if (song.order > largestOrder) {
+        largestOrder = song.order;
+      }
+    }
+    return {
+      ...playlist,
+      songList: playlist.songList
+        // then assign an order for all songs without one;
+        .map(song => {
+          if (!song.order) {
+            songsWithoutOrder -= 1;
+            return {
+              ...song,
+              order: largestOrder + songsWithoutOrder,
+            };
+          }
+          return song;
+        })
+        // and sort by order
+        .sort((a, b) => a.order! - b.order!),
+    };
+  }
+  // for any other sorting methods, first re-apply order to all songs
+  const songListLength = playlist.songList.length;
+  playlist.songList.forEach(
+    (song, index) => (song.order = songListLength - index)
+  );
+  switch (sort) {
+    case SORT_OPTIONS.TITLE:
+      return {
+        ...playlist,
+        songList: playlist.songList.sort((a, b) =>
+          a.parsedName.localeCompare(b.parsedName)
+        ),
+      };
+    case SORT_OPTIONS.ARTIST:
+      return {
+        ...playlist,
+        songList: playlist.songList.sort((a, b) =>
+          a.singer.localeCompare(b.singer)
+        ),
+      };
+    case SORT_OPTIONS.ALBUM:
+      return {
+        ...playlist,
+        songList: playlist.songList.sort((a, b) =>
+          (a.album ?? '').localeCompare(b.album ?? '')
+        ),
+      };
+    default:
+      return playlist;
+  }
+};
