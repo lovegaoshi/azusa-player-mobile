3e19cae93771bff0a6864322e0f7041243759d26
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-20 15:53:04 -0800

    feat: storage tests

---
+++ __tests__/storage/appstore.test.ts
@@ -0,0 +1,7 @@
+import { initPlayerObject } from '../../src/utils/ChromeStorage';
+
+test('load storage', async () => {
+  const loadedObject = await initPlayerObject();
+  // console.log(loadedObject);
+  expect(loadedObject.playlistIds.length).toBe(0);
+});
