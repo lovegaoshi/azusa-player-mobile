5eb54b74b400e2e8617a99d731efc8cc82e70a0e
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-02-01 10:05:55 -0800

    chore: kotlin migration

---
+++ android/app/src/main/java/com/noxplay/noxplayer/MainApplication.kt
@@ -0,0 +1,63 @@
+package com.noxplay.noxplayer
+
+import android.app.Application
+import android.content.res.Configuration
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+import com.microsoft.codepush.react.CodePush
+import expo.modules.ApplicationLifecycleDispatcher.onApplicationCreate
+import expo.modules.ApplicationLifecycleDispatcher.onConfigurationChanged
+import expo.modules.ReactNativeHostWrapper
+
+class MainApplication : Application(), ReactApplication {
+    override val reactNativeHost: ReactNativeHost =
+        ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
+            // 2. Override the getJSBundleFile method in order to let
+            // the CodePush runtime determine where to get the JS
+            // bundle location from on each app start
+            override fun getJSBundleFile(): String? {
+                return CodePush.getJSBundleFile()
+            }
+
+            override fun getUseDeveloperSupport(): Boolean {
+                return BuildConfig.DEBUG
+            }
+
+            override fun getPackages(): List<ReactPackage> {
+                val packages: MutableList<ReactPackage> = PackageList(this).packages
+                // Packages that cannot be autolinked yet can be added manually here, for example:
+                // packages.add(new MyReactNativePackage());
+                packages.add(NoxPackage())
+                return packages
+            }
+
+            override fun getJSMainModuleName(): String {
+                return "index"
+            }
+
+            override val isNewArchEnabled: Boolean
+                get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean
+                get() = BuildConfig.IS_HERMES_ENABLED
+        })
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this,  /* native exopackage */false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        onApplicationCreate(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        onConfigurationChanged(this, newConfig)
+    }
+}
