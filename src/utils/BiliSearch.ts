a991f8c3f77004457ddb5475a2972e776f77cb27
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-03-12 11:32:50 -0700

    chore: videoinfo refactor

--- src/utils/BiliSearch.ts
+++ src/utils/BiliSearch.ts
@@ -6,7 +6,7 @@ import biliBangumiFetch from './mediafetch/biliBangumi';
 import biliAudioAMFetch from './mediafetch/biliAudioAM';
 import biliAudioColleFetch from './mediafetch/biliAudioColle';
 import biliAudioSimilarFetch from './mediafetch/biliAudioSimilar';
-import bilivideoFetch from './mediafetch/bilivideo';
+import bilivideoFetch from './mediafetch/bilivideo2';
 import biliseriesFetch from './mediafetch/biliseries';
 import bilicolleFetch from './mediafetch/bilicolle';
 import bilifavlistFetch from './mediafetch/bilifavlist';
