e91969da881c33bcf628133922ab7349a9a5daa0
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-04-22 12:22:54 -0700

    feat: biliMusicTop test

--- __tests__/mediafetch/biliDynamic.test.ts
+++ __tests__/mediafetch/biliDynamic.test.ts
@@ -1,5 +1,5 @@
 import { fetchDynamic } from '../../src/utils/mediafetch/biliDynamic';
-test('bilicolle', async () => {
+test('biliDynamic', async () => {
   const content = await fetchDynamic();
   // console.log(content);
   expect(Object.values(content)[0][0]?.id).not.toBeNull();
