04e0ce842edbee9eb41ea5a30f7f90b602297854
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-02 09:54:36 -0700

    chore: refactor select colorscheme button

---
+++ src/components/setting/appearances/NoWeebButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import GenericDialog from '@components/dialogs/GenericDialog';
+
+import { SettingListItem, RenderSetting } from '../useRenderSetting';
