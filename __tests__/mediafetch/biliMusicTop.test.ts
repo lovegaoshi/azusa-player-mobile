54b90e7e456f89752bc55d6093505e109f39b982
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-14 10:32:06 -0700

    fix: biliMusicTop

--- __tests__/mediafetch/biliMusicTop.test.ts
+++ __tests__/mediafetch/biliMusicTop.test.ts
@@ -1,6 +1,6 @@
-import { fetchMusicTop } from '../../src/utils/mediafetch/biliMusicTop';
+import { fetchCurrentMusicTop } from '../../src/utils/mediafetch/biliMusicTop';
 test('biliMusicTop', async () => {
-  const content = await fetchMusicTop();
+  const content = await fetchCurrentMusicTop();
   // console.log(content);
   expect(content[0].bvid).not.toBeNull();
 }, 50000);
