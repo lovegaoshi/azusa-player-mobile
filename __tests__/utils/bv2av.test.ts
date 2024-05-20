d56e9fc36ff613ac5415d3203bfb0cf93401512c
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-20 09:08:46 -0700

    feat: bv2av test

---
+++ __tests__/utils/bv2av.test.ts
@@ -0,0 +1,12 @@
+import bfetch from '../../src/utils/BiliFetch';
+import { bv2av } from '../../src/utils/bv2av';
+const API =
+  'https://api.bilibili.com/x/centralization/interface/music/comprehensive/web/rank?pn=1&ps=100';
+
+test('bv2av', async () => {
+  const req = await bfetch(API);
+  const json = await req.json();
+  json.data.list.forEach((v: any) => {
+    expect(bv2av(v.bvid)).toBe(Number(v.aid));
+  });
+}, 10000);
