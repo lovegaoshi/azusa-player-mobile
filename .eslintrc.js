abc3fdcddd960f6ae1c99513e489b92a58220341
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-22 12:37:19 -0700

    Revert "chore: fix eslint9"
    This reverts commit 51fe0b33292c13c5eee16992516a109ebb545267.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    './node_modules/gts/',
+  ],
+  parserOptions: {
+    ecmaVersion: 2017, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  plugins: ['prettier'],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/display-name': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+  },
+  settings: {
+    react: { version: 'detect' },
+    // 'import/ignore': ['node_modules/react-native/index\\.js$'], doesnt work anymore..?
+    'import/ignore': ['react-native'],
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+};
