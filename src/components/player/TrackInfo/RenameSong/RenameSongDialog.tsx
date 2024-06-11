57509ea18777d849694731688385817d4a09465d
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-06-11 16:26:48 -0700

    chore: ref refactor

--- src/components/player/TrackInfo/RenameSong/RenameSongDialog.tsx
+++ src/components/player/TrackInfo/RenameSong/RenameSongDialog.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Button, Dialog, Portal } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
-import PortaledInput, {
-  PortalInputRef,
-} from '@components/dialogs/PortaledInput';
+import NoxInput from '@components/dialogs/NoxInput';
 
 interface Props {
   visible: boolean;
@@ -12,40 +10,46 @@ interface Props {
   onSubmit?: (rename: string) => void;
 }
 
-export default ({
+const SongDialog = ({
   visible,
   song,
   onClose = () => undefined,
   onSubmit = () => undefined,
 }: Props) => {
   const { t } = useTranslation();
-  const inputRef = React.useRef<PortalInputRef>();
+  const [text, setText] = React.useState(song.name);
 
   const handleClose = () => {
     onClose();
   };
   const handleSubmit = () => {
-    onSubmit(inputRef.current?.name ?? '');
+    onSubmit(text);
   };
 
   return (
-    <Portal>
-      <Dialog visible={visible} onDismiss={handleClose}>
-        <Dialog.Title>{t('RenameSongDialog.title', { song })}</Dialog.Title>
-        <Dialog.Content>
-          <PortaledInput
-            handleSubmit={handleSubmit}
-            ref={inputRef}
-            label={'RenameSongDialog.label'}
-            defaultName={song.name}
-          />
-        </Dialog.Content>
+    <Dialog visible={visible} onDismiss={handleClose}>
+      <Dialog.Title>{t('RenameSongDialog.title', { song })}</Dialog.Title>
+      <Dialog.Content>
+        <NoxInput
+          handleSubmit={handleSubmit}
+          label={t('RenameSongDialog.label')}
+          text={text}
+          setText={setText}
+        />
+      </Dialog.Content>
 
-        <Dialog.Actions>
-          <Button onPress={handleClose}>{t('Dialog.cancel')}</Button>
-          <Button onPress={handleSubmit}>{t('Dialog.ok')}</Button>
-        </Dialog.Actions>
-      </Dialog>
+      <Dialog.Actions>
+        <Button onPress={handleClose}>{t('Dialog.cancel')}</Button>
+        <Button onPress={handleSubmit}>{t('Dialog.ok')}</Button>
+      </Dialog.Actions>
+    </Dialog>
+  );
+};
+
+export default (p: Props) => {
+  return (
+    <Portal>
+      <SongDialog {...p} />
     </Portal>
   );
 };
