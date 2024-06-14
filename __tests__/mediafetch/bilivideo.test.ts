2aa1ad362b6b2349cf94a9b7161ee199ede71498
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-06-13 21:31:20 -0700

    fix: test
    wtf

--- __tests__/mediafetch/bilivideo.test.ts
+++ __tests__/mediafetch/bilivideo.test.ts
@@ -4,13 +4,13 @@ import fetcher, {
 test('bilivideo', async () => {
   const content = await fetcher.regexFetch({
     reExtracted: fetcher.regexSearchMatch.exec(
-      'https://www.bilibili.com/video/BV1cK42187AE/'
+      'https://www.bilibili.com/video/BV1WJ4m1H7XT'
     )!,
   });
   // console.log(content);
   expect(content?.songList[0]?.id).not.toBeNull();
 
-  const playurl = await fetchVideoPlayUrlPromise({ bvid: 'BV1cK42187AE' });
+  const playurl = await fetchVideoPlayUrlPromise({ bvid: 'BV1WJ4m1H7XT' });
   // console.log(playurl);
 
   expect(playurl.url).not.toBeNull();
