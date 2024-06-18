d2f66510fe3e474892628349965c4023350fc7dc
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-06-17 22:33:27 -0700

    fix: paginated fetch

--- src/utils/mediafetch/bilicolle.ts
+++ src/utils/mediafetch/bilicolle.ts
@@ -14,7 +14,7 @@ import { biliShazamOnSonglist } from './bilishazam';
 import { fetchBiliPaginatedAPI } from './paginatedbili';
 
 const URL_BILICOLLE_INFO =
-  'https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid={mid}&season_id={sid}&sort_reverse=true&page_num={pn}&page_size=100';
+  'https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid={mid}&season_id={sid}&sort_reverse=false&page_num={pn}&page_size=100';
 
 const fetchBiliColleList = (
   mid: string,
