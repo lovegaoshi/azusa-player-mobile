4352129f3e42480968844eb4e65658a14b6fd1d2
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-03 10:01:59 -0700

    chore: revert ytbi init
    too slow. whomp whomp.

--- src/hooks/useSetupPlayer.ts
+++ src/hooks/useSetupPlayer.ts
@@ -12,7 +12,6 @@ import useInitializeStore from '@stores/initializeStores';
 import { IntentData } from '@enums/Intent';
 import { useNoxSetting } from '@stores/useApp';
 import usePlayStore from './usePlayStore';
-import { awaitYtbiSetup } from '../utils/mediafetch/ytbi';
 
 const { NoxAndroidAutoModule } = NativeModules;
 
@@ -55,7 +54,6 @@ export default ({ intentData }: NoxComponent.AppProps) => {
       if (unmounted) return;
       setPlayerReady(true);
       if (unmounted) return;
-      awaitYtbiSetup();
       checkPlayStoreUpdates();
       const currentQueue = getCurrentTPQueue();
       const findCurrentSong = currentQueue.find(
