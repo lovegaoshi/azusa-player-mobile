5828bea585605f49d719b13b84d5229e6fe8c07c
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-05 11:36:05 -0700

    feat: ytb live (streamed)

---
+++ src/utils/mediafetch/ytbLive.ts
@@ -0,0 +1,21 @@
+import ytbVideoFetch from '@utils/mediafetch/ytbvideo';
+import logger from '../Logger';
+
+const regexFetch = async ({
+  reExtracted,
+}: NoxNetwork.RegexFetchProps): Promise<NoxNetwork.NoxRegexFetch> => {
+  const audioInfo = await ytbVideoFetch.regexFetch({ reExtracted });
+  if (audioInfo.songList.length == 0) {
+    logger.warn(
+      `[ytblive] fetch failed on ${reExtracted[0]} assuming its a video`
+      // TODO: resolve streaming url here
+    );
+  }
+  return audioInfo;
+};
+
+export default {
+  //https://www.youtube.com/live/v2gYvMIc_XI
+  regexSearchMatch: /youtu.*\/live\/([A-Za-z0-9_-]{11})/,
+  regexFetch,
+};
