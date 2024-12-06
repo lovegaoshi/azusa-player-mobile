397cf24c4f8535c658c037bfdff8bdd042156a59
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-12-05 16:14:16 -0800

    fix: bilicolle

--- src/utils/BiliFetch.ts
+++ src/utils/BiliFetch.ts
@@ -2,7 +2,7 @@
 import Bottleneck from 'bottleneck';
 
 export const DEFAULT_UA =
-  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0';
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0';
 
 export const parseBodyParams = (body: any) => {
   const formBody = [];
--- src/utils/mediafetch/biliuser.ts
+++ src/utils/mediafetch/biliuser.ts
@@ -7,6 +7,7 @@ const API = 'https://api.bilibili.com/x/space/wbi/acc/info?mid={mid}';
 export const getBiliUser = async (mid: string) => {
   const res = await wbiQuery(
     API.replace('{mid}', mid) + `${getDm()}${await getWebid(mid)}`,
+    { headers: { Referer: 'https://space.bilibili.com/' } },
   );
   const json = await res.json();
   return json.data;
