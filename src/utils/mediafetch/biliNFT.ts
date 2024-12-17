d3818744ec4dd3b8a182a227be5c85832bcb5a52
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-12-17 08:57:38 -0800

    fix: biliNFT

--- src/utils/mediafetch/biliNFT.ts
+++ src/utils/mediafetch/biliNFT.ts
@@ -1,40 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import bfetch from '@utils/BiliFetch';
 
-// see https://github.com/lovegaoshi/biliNFT
+// see https://github.com/SocialSisterYi/bilibili-API-collect/issues/1155
 
-const API =
-  'https://api.bilibili.com/x/vas/dlc_act/act/item/list?act_id={act_id}';
+const API = 'https://api.bilibili.com/x/vas/dlc_act/asset_bag?act_id={act_id}';
 
 interface Props {
   act_id: string;
   index: number | string;
-  resolver?: (json: any) => string;
+  resolver?: (v: any) => string;
+  filter?: (v: any) => any;
 }
 
 const biliNFTFetch = async ({
   act_id,
   index,
-  resolver = (json: any) => json.card_img,
+  resolver = (json: any) => json.card_item.card_img,
+  filter = (json: any) =>
+    json.item_list.filter((item: any) => item.card_item.card_name === index)[0],
 }: Props) => {
   const res = await bfetch(API.replace('{act_id}', act_id));
   const json = await res.json();
   if (typeof index === 'string') {
-    return resolver(
-      json.data.item_list.filter(
-        (item: { card_item: { card_name: string } }) =>
-          item.card_item.card_name === index,
-      )[0].card_item,
-    );
+    return resolver(filter(json.data));
   }
-  return resolver(json.data.item_list[index].card_item);
+  return resolver(json.data.item_list[index]);
 };
 
 const biliNFTVideoFetch = ({ act_id, index }: Props) =>
   biliNFTFetch({
     act_id,
     index,
-    resolver: (json: any) => json.video_list[0],
+    resolver: v => v.card_item.video_list[0],
   });
 
-export { biliNFTVideoFetch };
+const biliNFTRedeemFetch = ({ act_id, index }: Props) =>
+  biliNFTFetch({
+    act_id,
+    index,
+    resolver: v =>
+      v.card_item.card_type_info.content.animation.animation_video_urls[0],
+    filter: v =>
+      v.collect_list.filter((item: any) => item.redeem_item_name === index)[0],
+  });
+
+export { biliNFTVideoFetch, biliNFTRedeemFetch };
