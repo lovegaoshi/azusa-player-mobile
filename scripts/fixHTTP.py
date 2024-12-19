5c2fc3e7df91fee68864add4f3b506c810a26009
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-12-19 11:49:59 -0800

    chore: yarn dep

--- scripts/fixHTTP.py
+++ scripts/fixHTTP.py
@@ -1,7 +1,7 @@
 import os
 from dev_cleartext import fix_content
 
-if __name__ == '__main__':
+if __name__ == '__main__1':
     mfplugin_dir = './MusicFreePlugins/dist'
     for i in os.listdir(mfplugin_dir):
         index_js_path = os.path.join(mfplugin_dir, i, 'index.js')
