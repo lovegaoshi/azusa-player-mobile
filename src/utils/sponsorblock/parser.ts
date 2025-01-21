e1708290798fbc8a00cafc37e0d8de18509886c4
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-01-21 13:12:35 -0800

    feat: ytb sponsorblock

--- src/utils/sponsorblock/parser.ts
+++ src/utils/sponsorblock/parser.ts
@@ -1,10 +1,13 @@
 import { Source } from '@enums/MediaFetch';
 import { getSponsorBlock as getSponsorBlockBili } from './bilibili';
+import { getSponsorBlock as getSponsorYtb } from './ytb';
 
 export const getSponsorBlock = async (song: NoxMedia.Song) => {
   switch (song.source) {
     case Source.bilivideo:
       return getSponsorBlockBili(song);
+    case Source.ytbvideo:
+      return getSponsorYtb(song);
   }
   return [];
 };
