1765cfefe5284da446abb01858291a6c3c2e5b5d
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-03-28 17:00:23 -0700

    fix: artist page profile

--- src/utils/artistfetch/biliartist.ts
+++ src/utils/artistfetch/biliartist.ts
@@ -3,7 +3,7 @@ import { fetchBiliChannelList } from '../mediafetch/bilichannel';
 import getBiliUser from '../Bilibili/biliUserCard';
 import getBiliNotice from '../Bilibili/biliNotice';
 import { getListAsYTSongRowCard } from '../mediafetch/bililist';
-import { nFormatter } from '../Utils';
+import { i0hdslbHTTPResolve, nFormatter } from '../Utils';
 
 export interface ArtistFetch {
   profilePicURL: string;
@@ -32,7 +32,7 @@ export default async (mid: string): Promise<ArtistFetch> => {
   });
   const userInfo = await getBiliUser(mid);
   return {
-    profilePicURL: userInfo?.space?.s_img ?? '',
+    profilePicURL: i0hdslbHTTPResolve(userInfo?.space?.s_img) ?? '',
     ProfilePlaySongs,
     artistName: userInfo?.card.name ?? '',
     attestation: userInfo?.card.official?.title ?? '',
