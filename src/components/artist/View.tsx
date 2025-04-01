40fbf72fbcbf80ddd0289107d2c8e1ae214b26b0
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-04-01 07:59:33 -0700

    chore: refactor

--- src/components/artist/View.tsx
+++ src/components/artist/View.tsx
@@ -17,7 +17,6 @@ import Animated, {
 import { IconButton, Text, ActivityIndicator } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
 
-import FlexView from '@components/commonui/FlexViewNewArch';
 import { NoxRoutes } from '@enums/Routes';
 import useArtist from '@stores/explore/artist';
 import { YTSongRow } from '../explore/SongRow';
@@ -78,137 +77,127 @@ export default ({ navigation }: NoxComponent.StackNavigationProps) => {
 
   if (loading) {
     return (
-      <FlexView>
-        <View style={backgroundStyle}>
-          <View style={mStyles.indicatorContainer} />
-          <Text variant="titleLarge" style={styles.centerText}>
-            {t('Artist.loading', { name: song?.singer })}
-          </Text>
-          <View style={mStyles.indicatorContainer} />
-          <ActivityIndicator size={100} />
-        </View>
-      </FlexView>
+      <View style={backgroundStyle}>
+        <View style={mStyles.indicatorContainer} />
+        <Text variant="titleLarge" style={styles.centerText}>
+          {t('Artist.loading', { name: song?.singer })}
+        </Text>
+        <View style={mStyles.indicatorContainer} />
+        <ActivityIndicator size={100} />
+      </View>
     );
   }
 
   if (result === undefined) {
     return (
-      <FlexView>
-        <ScrollView
-          style={[{ paddingLeft: 10 }, backgroundStyle]}
-          refreshControl={
-            <RefreshControl refreshing={false} onRefresh={onRefresh} />
-          }
-        >
-          <View style={mStyles.indicatorContainer} />
-          <Text variant="titleLarge">{t('Artist.errorTitle')}</Text>
-          <Text variant="bodyLarge">{t('Artist.errorContent')}</Text>
-        </ScrollView>
-      </FlexView>
+      <ScrollView
+        style={[{ paddingLeft: 10 }, backgroundStyle]}
+        refreshControl={
+          <RefreshControl refreshing={false} onRefresh={onRefresh} />
+        }
+      >
+        <View style={mStyles.indicatorContainer} />
+        <Text variant="titleLarge">{t('Artist.errorTitle')}</Text>
+        <Text variant="bodyLarge">{t('Artist.errorContent')}</Text>
+      </ScrollView>
     );
   }
 
   return (
-    <FlexView>
-      <View style={backgroundStyle}>
-        <View style={mStyles.headerContainer}>
-          <Animated.View
-            style={[
-              {
-                width: dimension.width,
-                height: 60,
-                position: 'absolute',
-                backgroundColor: playerStyle.colors.primaryContainer,
-              },
-              animatedHeaderStyle,
-            ]}
+    <View style={backgroundStyle}>
+      <View style={mStyles.headerContainer}>
+        <Animated.View
+          style={[
+            {
+              width: dimension.width,
+              height: 60,
+              position: 'absolute',
+              backgroundColor: playerStyle.colors.primaryContainer,
+            },
+            animatedHeaderStyle,
+          ]}
+        />
+        <IconButton
+          iconColor={playerStyle.colors.primary}
+          icon={'arrow-left'}
+          onPress={() => navigation.navigate(NoxRoutes.Playlist)}
+          size={30}
+        />
+        <Animated.View
+          style={[{ justifyContent: 'center' }, animatedHeaderStyle]}
+        >
+          <Text variant="titleLarge">{result.artistName}</Text>
+        </Animated.View>
+        <View style={styles.flex} />
+        <View style={styles.topBarIcons}>
+          <IconButton
+            iconColor={playerStyle.colors.primary}
+            icon={'playlist-plus'}
+            onPress={() =>
+              setExternalSearchText(
+                result.playURL ?? goToArtistExternalPage(song)!,
+              )
+            }
+            size={30}
           />
           <IconButton
             iconColor={playerStyle.colors.primary}
-            icon={'arrow-left'}
-            onPress={() => navigation.navigate(NoxRoutes.Playlist)}
+            icon={'share'}
+            onPress={() =>
+              Linking.openURL(result.shareURL ?? goToArtistExternalPage(song)!)
+            }
             size={30}
           />
-          <Animated.View
-            style={[{ justifyContent: 'center' }, animatedHeaderStyle]}
-          >
-            <Text variant="titleLarge">{result.artistName}</Text>
-          </Animated.View>
-          <View style={styles.flex} />
-          <View style={styles.topBarIcons}>
-            <IconButton
-              iconColor={playerStyle.colors.primary}
-              icon={'playlist-plus'}
-              onPress={() =>
-                setExternalSearchText(
-                  result.playURL ?? goToArtistExternalPage(song)!,
-                )
-              }
-              size={30}
-            />
-            <IconButton
-              iconColor={playerStyle.colors.primary}
-              icon={'share'}
-              onPress={() =>
-                Linking.openURL(
-                  result.shareURL ?? goToArtistExternalPage(song)!,
-                )
-              }
-              size={30}
-            />
-          </View>
         </View>
-        <ScrollView
-          onScroll={e => {
-            scrollYOffset.value = e.nativeEvent.contentOffset.y;
-            scrollYHeight.value = e.nativeEvent.layoutMeasurement.height;
-          }}
-        >
-          <Animated.View style={animatedArtistHeaderStyle}>
-            <Image
-              style={{
-                width: dimension.width,
-                height: Math.max(hideAt.value, 200),
-              }}
-              source={result.profilePicURL}
-            />
-            <View style={mStyles.titleTextContainer}>
-              <Text variant="headlineLarge">{result.artistName}</Text>
-              {result.subscribers.length > 0 && (
-                <Text>
-                  {t('Artist.subscribers', { c: result.subscribers })}
-                </Text>
-              )}
-            </View>
-          </Animated.View>
-          {result.ProfilePlaySongs.length > 0 && (
-            <BiliSongsArrayTabCard
-              songs={result.ProfilePlaySongs}
-              title={t('Artist.latest')}
-            />
-          )}
-          {result.topSongs.length > 0 && (
-            <BiliSongsArrayTabCard
-              songs={result.topSongs}
-              title={t('Artist.top')}
-            />
-          )}
-          {result.albums
-            .filter(v => v.data[0])
-            .map(v => (
-              <YTSongRow
-                key={v.data[0].cover}
-                songs={v.data}
-                title={t(v.name ?? 'Albums')}
-              />
-            ))}
-          <View style={ItemSelectStyles.skinItemTextContainer}>
-            <Text>{result.sign}</Text>
-            <Text>{result.aboutString}</Text>
-          </View>
-        </ScrollView>
       </View>
-    </FlexView>
+      <ScrollView
+        onScroll={e => {
+          scrollYOffset.value = e.nativeEvent.contentOffset.y;
+          scrollYHeight.value = e.nativeEvent.layoutMeasurement.height;
+        }}
+      >
+        <Animated.View style={animatedArtistHeaderStyle}>
+          <Image
+            style={{
+              width: dimension.width,
+              height: Math.max(hideAt.value, 200),
+            }}
+            source={result.profilePicURL}
+          />
+          <View style={mStyles.titleTextContainer}>
+            <Text variant="headlineLarge">{result.artistName}</Text>
+            {result.subscribers.length > 0 && (
+              <Text>{t('Artist.subscribers', { c: result.subscribers })}</Text>
+            )}
+          </View>
+        </Animated.View>
+        {result.ProfilePlaySongs.length > 0 && (
+          <BiliSongsArrayTabCard
+            songs={result.ProfilePlaySongs}
+            title={t('Artist.latest')}
+          />
+        )}
+        {result.topSongs.length > 0 && (
+          <BiliSongsArrayTabCard
+            songs={result.topSongs}
+            title={t('Artist.top')}
+          />
+        )}
+        {result.albums
+          .filter(v => v.data[0])
+          .map(v => (
+            <YTSongRow
+              key={v.data[0].cover}
+              songs={v.data}
+              title={t(v.name ?? 'Albums')}
+            />
+          ))}
+        <View style={ItemSelectStyles.skinItemTextContainer}>
+          <Text>{result.sign}</Text>
+          <Text>{result.aboutString}</Text>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
--- src/components/playlist/View.tsx
+++ src/components/playlist/View.tsx
@@ -5,23 +5,20 @@ import { styles } from '../style';
 import BiliSearchbar from './BiliSearch/BiliSearchbar';
 import SongList from './SongList/SongList';
 import { useNoxSetting } from '@stores/useApp';
-import FlexView from '@components/commonui/FlexViewNewArch';
 
 const Playlist = () => {
   const playerStyle = useNoxSetting(state => state.playerStyle);
 
   return (
-    <FlexView>
-      <View
-        style={[
-          styles.contentContainer,
-          { backgroundColor: playerStyle.customColors.maskedBackgroundColor },
-        ]}
-      >
-        <BiliSearchbar onSearched={console.log} />
-        <SongList />
-      </View>
-    </FlexView>
+    <View
+      style={[
+        styles.contentContainer,
+        { backgroundColor: playerStyle.customColors.maskedBackgroundColor },
+      ]}
+    >
+      <BiliSearchbar onSearched={console.log} />
+      <SongList />
+    </View>
   );
 };
 
