e13810eb9e37f99171f29fc3d0c2e9b540905672
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-04-01 13:16:00 -0700

    feat: OTA

--- src/utils/OTAUpdate.ts
+++ src/utils/OTAUpdate.ts
@@ -2,8 +2,14 @@ import hotUpdate from 'react-native-ota-hot-update';
 import { Alert, Platform } from 'react-native';
 import ReactNativeBlobUtil from 'react-native-blob-util';
 
+const url =
+  Platform.OS === 'ios'
+    ? 'https://github.com/lovegaoshi/azusa-player-mobile/raw/refs/heads/OTA/main.jsbundle.zip'
+    : 'https://github.com/lovegaoshi/azusa-player-mobile/raw/refs/heads/OTA/index.android.bundle.zip';
+
 export const onCheckGitVersion = () =>
   hotUpdate.downloadBundleUri(ReactNativeBlobUtil, url, undefined, {
+    progress: console.log,
     updateSuccess: () => {
       console.log('Update successful!');
     },
