e8738bb561bdb61df107c360b88f901c95673f81
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-04-29 13:13:06 -0700

    fix: lint

--- src/hooks/useSongOperations.ts
+++ src/hooks/useSongOperations.ts
@@ -5,14 +5,12 @@ const useSongOperations = () => {
   const setExternalSearchText = useNoxSetting(
     state => state.setExternalSearchText,
   );
-  const setSongMenuVisible = useNoxSetting(state => state.setSongMenuVisible);
 
   const startRadio = (song: NoxMedia.Song) => {
     const url = getRadioUrl(song);
     if (url) {
       setExternalSearchText(url);
     }
-    setSongMenuVisible(false);
   };
 
   return { startRadio };
