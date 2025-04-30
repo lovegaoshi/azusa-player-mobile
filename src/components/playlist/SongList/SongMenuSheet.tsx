ccae38ebfbc7e3fdd6afa49cb7e92ca4a7e3ab71
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-04-29 18:08:26 -0700

    Update SongMenuSheet.tsx

--- src/components/playlist/SongList/SongMenuSheet.tsx
+++ src/components/playlist/SongList/SongMenuSheet.tsx
@@ -164,7 +164,7 @@ export default ({ usePlaylist, prepareForLayoutAnimationRender }: Props) => {
           getSongOnClick={() => selectedSongs()[0]}
           onSubmit={renameSong}
           showSheet={showSheet}
-        />{' '}
+        />
         <SheetIconButton
           text={t('SongOperations.songSearchInPlaylistTitle')}
           icon={'text-search'}
