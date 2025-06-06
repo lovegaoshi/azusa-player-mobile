8ff5f4f25e42a6d0178e8f89bc375fb908ad1c08
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-06-06 06:29:23 -0700

    chore: refactor children out of mainbackground

--- src/components/background/AccentColorBackground.tsx
+++ src/components/background/AccentColorBackground.tsx
@@ -3,8 +3,18 @@ import { View } from 'react-native';
 
 import useAccentColor from '@hooks/useAccentColor';
 
-export default ({ children }: { children: React.JSX.Element }) => {
+export default () => {
   const { backgroundColor } = useAccentColor();
 
-  return <View style={{ backgroundColor, flex: 1 }}>{children}</View>;
+  return (
+    <View
+      style={{
+        backgroundColor,
+        flex: 1,
+        position: 'absolute',
+        width: 9999,
+        height: 8888,
+      }}
+    />
+  );
 };
