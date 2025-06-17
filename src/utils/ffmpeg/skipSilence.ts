bd7486b374e1b7acee7be181367f4c8fc046e636
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-06-17 14:26:39 -0700

    feat: nox skip silence

--- src/utils/ffmpeg/skipSilence.ts
+++ src/utils/ffmpeg/skipSilence.ts
@@ -1,10 +1,13 @@
 import { getABRepeatRaw } from '../db/sqlAPI';
 import { setABRepeat } from '../db/sqlStorage';
+import logger from '../Logger';
 import { probeLoudness } from './ffmpeg';
 
 export const setNoxSkipSilence = async (path: string, song: NoxMedia.Song) => {
+  logger.debug(`[SkipSilence] now starting FFMPEG skip silence for ${song.id}`);
   const abrepeat = await getABRepeatRaw(song.id);
   if (abrepeat?.a !== undefined || abrepeat?.b !== undefined) {
+    logger.debug(`[SkipSilence] ABrepeat exists; skipping...`);
     return;
   }
   const newABRepeat = await probeLoudness(path);
