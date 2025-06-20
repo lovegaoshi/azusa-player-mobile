90977eb8c33543f0ac7b525af5b2a73043373441
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-06-20 14:51:32 -0700

    fix: flex view height responsive on miniplayer

--- src/components/commonui/FlexViewNewArch.tsx
+++ src/components/commonui/FlexViewNewArch.tsx
@@ -3,6 +3,9 @@ import { View, ViewStyle } from 'react-native';
 
 import { styles } from '@components/style';
 import { isOldArch } from '@utils/RNUtils';
+import { useMiniplayerHeight } from '@contexts/MiniPlayerHeightContext';
+import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+import { MinPlayerHeight } from '../miniplayer/Constants';
 
 interface Props {
   children: React.JSX.Element;
@@ -13,16 +16,29 @@ interface Props {
  * a view of flex:1 for new arch, resolves resizing issues
  */
 export default ({ children, noFlex, style }: Props) => {
+  // do NOT change noFlex OTF.
   if (isOldArch() && !noFlex) {
     return <View style={[styles.flex, style]}>{children}</View>;
   }
 
   const [initHeight, setInitHeight] = useState(0);
   const largestHeight = useRef(-1);
+  const miniplayerHeight = useMiniplayerHeight();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    if (miniplayerHeight.value < MinPlayerHeight) {
+      return { flex: 1, height: undefined };
+    }
+    return {};
+  });
 
   return (
-    <View
-      style={[initHeight === 0 ? styles.flex : { height: initHeight }, style]}
+    <Animated.View
+      style={[
+        initHeight === 0 ? styles.flex : { height: initHeight },
+        style,
+        animatedStyle,
+      ]}
       onLayout={e => {
         if (initHeight > 0 || e.nativeEvent.layout.height === 0) {
           return;
@@ -35,6 +51,6 @@ export default ({ children, noFlex, style }: Props) => {
       }}
     >
       {children}
-    </View>
+    </Animated.View>
   );
 };
