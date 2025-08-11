62148724d2fb65e905d47851dccbf4f62bd1acab
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-08-11 09:47:40 -0700

    feat: inapp update notifee

--- src/hooks/usePlayStore.ts
+++ src/hooks/usePlayStore.ts
@@ -2,11 +2,15 @@ import SpInAppUpdates, {
   IAUUpdateKind,
   StartUpdateOptions,
 } from 'sp-react-native-in-app-updates';
+import notifee from '@notifee/react-native';
+import i18n from 'i18next';
 
 // eslint-disable-next-line import/no-unresolved
 import { APPSTORE } from '@env';
 import logger from '@utils/Logger';
 import { isAndroid } from '@utils/RNUtils';
+
+const ID = 'APM-inapp-update-notification';
 
 export default () => {
   const inAppUpdates = new SpInAppUpdates(
@@ -20,13 +24,43 @@ export default () => {
       const result = await inAppUpdates.checkNeedsUpdate();
       if (result.shouldUpdate) {
         let updateOptions: StartUpdateOptions = {};
+        let channel: string = '';
         if (isAndroid) {
+          // Create a channel (required for Android)
+          channel = await notifee.createChannel({
+            id: 'APM-inapp-update',
+            name: 'APM In App Update Notification',
+          });
           // android only, on iOS the user will be promped to go to your app store page
           updateOptions = {
             updateType: IAUUpdateKind.FLEXIBLE,
           };
         }
-        inAppUpdates.startUpdate(updateOptions); // https://github.com/SudoPlz/sp-react-native-in-app-updates/blob/master/src/types.ts#L78
+        inAppUpdates.addStatusUpdateListener(async s => {
+          const progressOptions =
+            s.bytesDownloaded >= s.totalBytesToDownload
+              ? undefined
+              : {
+                  max: 1,
+                  current: s.bytesDownloaded / s.totalBytesToDownload,
+                };
+          const notificationId = await notifee.displayNotification({
+            id: ID,
+            title: i18n.t('InAppUpdate.Title'),
+            body: i18n.t('InAppUpdate.body'),
+            android: {
+              channelId: channel,
+              progress: progressOptions,
+            },
+          });
+          if (s.bytesDownloaded >= s.totalBytesToDownload) {
+            inAppUpdates.installUpdate();
+            setTimeout(() => {
+              notifee.cancelNotification(notificationId);
+            }, 5000);
+          }
+        });
+        await inAppUpdates.startUpdate(updateOptions); // https://github.com/SudoPlz/sp-react-native-in-app-updates/blob/master/src/types.ts#L78
       }
     } catch {
       logger.error('[PlayStore] checkPlayStoreUpdates failed');
