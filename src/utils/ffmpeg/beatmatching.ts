1e9b27ab124e31dce299e2819e985244b32aae07
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-08-22 10:07:24 -0700

    feat: beat matching

--- src/utils/ffmpeg/beatmatching.ts
+++ src/utils/ffmpeg/beatmatching.ts
@@ -1,17 +1,52 @@
-/**
 interface IBeatMatch {
   song1Beats: number[];
   song2Beats: number[];
   crossfadeInterveral: number;
   song1Duration?: number;
-  song1FadeOut?: number;
-  song2FadeIn?: number;
+  bRepeat?: number;
+  aRepeat?: number;
 }
-export const beatMatch = ({}: IBeatMatch) => {
-  // first determine song1's fade out range.
+
+const calculateBeatDiff = (beats: number[]) => {
+  const difference = beats.map((beat, index) => beats[index + 1] - beat);
+  return difference.slice(0, difference.length - 1);
 };
 
- */
+export const beatMatch = ({
+  song1Beats,
+  song2Beats,
+  bRepeat,
+  aRepeat,
+  crossfadeInterveral,
+}: IBeatMatch): [number, number] => {
+  // first get the diff that would be used to match
+  const song1BeatDiff = calculateBeatDiff(song1Beats);
+  const song2BeatDiff = calculateBeatDiff(song2Beats);
+  // here a beat at song1Beats index x should match for song1BeatDiff at x-1
+
+  // get song1Fadeout and get the last beat index i should match at.
+  // eg [0, 1,2,3,4,5] my brepeat is 4.5; it should find 4
+  // so i find the first element larget than brepeat then subtract 1; else i want to return this length (5)
+  const song1LastIndex = bRepeat
+    ? song1Beats.findIndex(beat => beat > bRepeat) - 1
+    : song1Beats.length;
+  // eg [0, 1,2,3,4,5] my arepeat is 2.5; it should find 3
+  const song2FirstIndex = aRepeat
+    ? song2Beats.findIndex(beat => beat > aRepeat)
+    : 0;
+
+  // no beat matching: lets just take the first beat within the crossfade interval and jam it
+  // to the first beat of the other song
+  const song1Trans = song1Beats[song1LastIndex] - crossfadeInterveral;
+  const song1Transition = song1Beats.findIndex(b => b > song1Trans);
+  console.log(
+    'beat map',
+    song1Beats.slice(song1Transition),
+    song2Beats.slice(song2FirstIndex, song2FirstIndex + 10),
+  );
+  return [song1Beats[song1Transition], song2Beats[song2FirstIndex]];
+};
+
 import { getSongBeat } from '../db/sqlAPI';
 import { setSongBeat } from '../db/sqlStorage';
 import logger from '../Logger';
