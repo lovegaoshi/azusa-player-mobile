e3223105d257d1555c00bc4ddac38b9bc3fcff65
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-09-15 21:24:28 -0700

    fix: ytbi RN 0.81

--- scripts/fixHTTP.py
+++ scripts/fixHTTP.py
@@ -1,7 +1,15 @@
 import os
+import sys
+
 from dev_cleartext import fix_content
 
-if __name__ == '__main__1':
+if __name__ == '__main__':
+
+    fix_content('./node_modules/youtubei.js/package.json', lambda line: line.replace(
+        '"react-native": "./dist/src/platform/react-native.js",', '"react-native": "./bundle/react-native.js",'
+    ))
+
+    sys.exit(0)
     mfplugin_dir = './MusicFreePlugins/dist'
     for i in os.listdir(mfplugin_dir):
         index_js_path = os.path.join(mfplugin_dir, i, 'index.js')
