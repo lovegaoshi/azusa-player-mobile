6f91b3cf11a67839581303bf0ed8d78456ec8670
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-09-24 09:19:51 -0700

    fix: ytbi
    for now use web instead of ios

--- src/utils/mediafetch/ytbi.ts
+++ src/utils/mediafetch/ytbi.ts
@@ -42,7 +42,7 @@ const createYtClient = () =>
     retrieve_player: true,
     enable_session_cache: false,
     generate_session_locally: false,
-    client_type: ClientType.IOS,
+    client_type: ClientType.WEB_EMBEDDED,
     //cookie,
   });
 
