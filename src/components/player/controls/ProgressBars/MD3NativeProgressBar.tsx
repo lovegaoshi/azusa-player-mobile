2b82366922a4a96b18bf6c1f978a0a26d02f669a
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2026-06-05 17:07:26 -0700

    feat: md3 slider

--- src/components/player/controls/ProgressBars/MD3NativeProgressBar.tsx
+++ src/components/player/controls/ProgressBars/MD3NativeProgressBar.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 import { useProgress } from 'react-native-track-player';
 import { WavySlider as Slider } from 'expo-wavy-slider';
 import { scheduleOnRN } from 'react-native-worklets';
 import { useStore } from 'zustand';
 import { useSharedValue, withTiming } from 'react-native-reanimated';
+import { colord } from 'colord';
 
 import { useNoxSetting } from '@stores/useApp';
 import { TPSeek } from '@stores/RNObserverStore';
@@ -20,12 +21,19 @@ export default function SimpleProgressBar({
   onValueChange,
 }: ProgressBarProps) {
   const { position, duration } = useProgress(progressInterval, false);
+  const playerStyle = useNoxSetting(state => state.playerStyle);
   const enterSliding = useNoxSetting(state => state.enableMiniProgressSliding);
   const exitSliding = useNoxSetting(state => state.disableMiniProgressSliding);
   const fetchProgress = useStore(appStore, state => state.fetchProgress);
   const immediateShowPause = useNoxSetting(state => state.immediateShowPause);
   const waveHeight = useSharedValue(0);
   const playProgress = position / duration;
+
+  const bufferColor = useMemo(
+    () =>
+      colord(playerStyle.colors.primaryContainer).darken(0.04).toRgbString(),
+    [playerStyle.colors.primaryContainer],
+  );
 
   useEffect(() => {
     waveHeight.value = withTiming(immediateShowPause ? 0 : WaveHeight, {
@@ -39,6 +47,10 @@ export default function SimpleProgressBar({
       bufferedValue={
         (fetchProgress / 100 - playProgress) / (1 - playProgress) || 0
       }
+      colors={{
+        // inactiveTrackColor: playerStyle.colors.secondary,
+        bufferedTrackColor: bufferColor,
+      }}
       value={playProgress || 0}
       onValueChange={v => {
         'worklet';
