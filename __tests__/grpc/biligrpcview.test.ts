aa109b2d929ec4b2a3d03f62d8979a22d5e605a6
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2026-09-03 08:39:32 -0700

    feat: biligrpcview

--- __tests__/grpc/biligrpcview.test.ts
+++ __tests__/grpc/biligrpcview.test.ts
@@ -6,5 +6,6 @@ describe('bilibili view_pb protobuf tests', () => {
   test('fetches view using fetchBiliView utility', async () => {
     const reply = await fetchBiliView(BVID);
     expect(reply.bvid).toBe(BVID);
+    expect(reply.pages.length > 1).toBe(true);
   }, 30000);
 });
